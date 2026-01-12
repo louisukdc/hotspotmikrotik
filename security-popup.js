ec4c6c18e24091ba397348d89c2a77108c420f9a
Louis <louis.maximillian@student.ukdc.ac.id>
2026-01-12 11:07:56 +0700

    new update

--- security-popup.js
+++ security-popup.js
@@ -2,11 +2,40 @@ let popup = document.getElementById("security-popup");
 let overlay = document.getElementById("overlay");
 
 
-let slideIndex = 0;
+let slideIndex = 1;
+showDivs(slideIndex);
 const SLIDE_INTERVAL = 3000; 
 let slideshowTimer;
 let slideWrapper;
 
+
+function plusDivs(n) {
+    clearInterval(slideshowTimer); 
+    showDivs(slideIndex += n);
+    startSlideshow();
+}
+
+function showDivs(n) {
+  let i;
+  let x = document.getElementsByClassName("slide-image-grid");
+  
+  if (x.length === 0) return; 
+
+  
+  if (n > x.length) {slideIndex = 1} 
+  
+  if (n < 1) {slideIndex = x.length}
+  
+  
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+    x[i].classList.remove("fade"); 
+  }
+
+  
+  x[slideIndex-1].style.display = "block";  
+  x[slideIndex-1].classList.add("fade"); 
+}
 
 function showNextSlide() {
     const wrapperPopup = document.getElementById("slide-wrapper");
@@ -42,19 +71,13 @@ function showNextSlide() {
 
 
 function startSlideshow() {
+
     if (slideshowTimer) clearInterval(slideshowTimer);
 
-    slideWrapper = document.getElementById("slide-wrapper");   
-    slideWrapper = document.getElementById("slideShowGrid") || slideWrapper;
     
-    if (!slideWrapper) {
-        setTimeout(startSlideshow, 100);
-        return;
-    }
-
-    slideIndex = 0;
-    slideWrapper.style.transform = `translateX(0%)`;
-    slideshowTimer = setInterval(showNextSlide, SLIDE_INTERVAL);
+    slideshowTimer = setInterval(() => {
+        showDivs(slideIndex += 1);
+    }, SLIDE_INTERVAL);
 }
 
 
@@ -82,6 +105,7 @@ function closePopup() {
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {    
+    showDivs(slideIndex);
     startSlideshow();
     setTimeout(openPopup, 500); 
 });
